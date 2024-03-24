ef85b562345c012981aad80143551e84d354ea1f
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-03-24 22:08:10 +0100

    Added a bash script that initiates the log file

--- stuffs/initialing_setup.sh
+++ stuffs/initialing_setup.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-echo "Temperature, Humidity, Presure" > logs/Lag_weather.log
-echo "Temperature, Humidity, Presure" > logs/Abj_weather.log
-echo "Temperature, Humidity, Presure" > logs/Port_weather.log
+
+
+
+echo "Temperature, Humidity, Presure" > /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/weatherLog/Lag_weather.log
+echo "Temperature, Humidity, Presure" > /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/weatherLog/Abj_weather.log
+echo "Temperature, Humidity, Presure" > /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/weatherLog/Port_weather.log
 
 
 # * * * * * /bin/bash  /Users/nombauser/Desktop/GIT/my_git_repos/ETLWithBashScript/temperature_ETL.sh
 
 
-echo "Temperature, Humidity, Presure" > buffered_log/Lag_weather.csv
-echo "Temperature, Humidity, Presure" > buffered_log/Abj_weather.csv
-echo "Temperature, Humidity, Presure" > buffered_log/Port_weather.csv
+echo "Temperature, Humidity, Presure" > /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/buffered_log/Lag_weather.csv
+echo "Temperature, Humidity, Presure" > /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/buffered_log/Abj_weather.csv
+echo "Temperature, Humidity, Presure" > /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/buffered_log/Port_weather.csv
