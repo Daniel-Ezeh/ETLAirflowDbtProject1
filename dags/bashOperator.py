664b7d8db1a1beb506079b0619bb13684ee964a5
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-03-25 07:55:53 +0100

    Updating process

--- dags/bashOperator.py
+++ dags/bashOperator.py
@@ -1,28 +1,38 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-import airflow
-from airflow.operators.bash import BashOperator
-
-from datetime import datetime, timedelta
-from airflow import DAG
-import airflow
-from airflow.operators.bash import BashOperator
-
-default_args = {
-    'owner':"Dan4sure",
-    'retries': 5,
-    'retry_delay': timedelta(seconds=10)
-}
+# from datetime import datetime, timedelta
+# from airflow import DAG
+# import airflow
+# from airflow.operators.bash import BashOperator
+# from airflow.operators.empty import EmptyOperator
 
 
-with DAG(
-    dag_id = "my_first_dag_v4.1.1",
-    default_args = default_args,
-    description = "this is my first dag I am writing",
-    start_date = datetime(2024, 2, 25, 3),
-    schedule_interval = '*/10 * * * *'
-) as dag:
-    task1 = BashOperator(
-        task_id="bash_operator",
-        bash_command="echo hello world, this is the first task!"
-    )
+# from datetime import datetime, timedelta
+# from airflow import DAG
+# import airflow
+# from airflow.operators.bash import BashOperator
+
+# default_args = {
+#     'owner':"Dan4sure",
+#     'retries': 5,
+#     'retry_delay': timedelta(seconds=10)
+# }
+
+
+# with DAG(
+#     dag_id = "Extraction_1",
+#     default_args = default_args,
+#     description = "this is my first dag I am writing",
+#     start_date = datetime(2024, 2, 25, 3),
+#     schedule_interval = '*/10 * * * *'
+# ) as dag:
+    
+#     pass
+#     task1 = EmptyOperator(
+#         task_id="Start",
+#     )
+
+#     task2 = BashOperator(
+#         task_id="bash_operator",
+#         bash_command="bash /Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1/stuffs/temperature_ETL.sh"
+#     )
+
+# task1 >> task2
---
+++ stuffs/config.py
@@ -0,0 +1,4 @@
+# config.py
+
+API_KEY = 'aacaa216d10142c5a19104531242203'
+CITY = ["Lagos", "Abuja", "Port Harcourt"]
