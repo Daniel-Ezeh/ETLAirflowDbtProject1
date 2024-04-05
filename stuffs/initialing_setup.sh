1e97f4c0ae17b97be92b7e44fda6a174daab9cb9
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-04-05 15:26:26 +0100

    Added code for setting up product and sales bucket

--- stuffs/initialing_setup.sh
+++ stuffs/initialing_setup.sh
@@ -19,3 +19,9 @@ echo "Temperature, Humidity, Presure" > $DIR/stuffs/buffered_log/Port_weather.cs
 
 DIR=/Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1
 echo "customer_id, firstname, lastname, gender, email, phone, address, country, date_added" > $DIR/stuffs/customer_log/customers.csv
+
+DIR=/Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1
+echo "product_id, name, description, price" > $DIR/stuffs/customer_log/products.csv
+
+DIR=/Users/nombauser/Desktop/GIT/my_git_repos/ETLAirflowDbtProject1
+echo "sale_id, customer_id, product_id, quantity, sale_date" > $DIR/stuffs/customer_log/sales.csv
