d6892c55d622b2fe2a0ab91ebb152d68a100b81c
Daniel Ezeh <daniel.ezeh@nomba.com>
2024-04-11 20:54:10 +0100

    Added intermediate file

--- my_dbt/models/intermediate/int_customer_purchase.sql
+++ my_dbt/models/intermediate/int_customer_purchase.sql
@@ -1,22 +1,26 @@
-WITH customers AS (
+{{ confi }}
+
+WITH cc AS (
     SELECT 
-        customer_id
+        customer_id,
         first_name,
         last_name,
         gender,
         country
     FROM {{ ref("base_postgres_customers") }}
+    ORDER BY gender ASC
 ),
 
-sales AS (
+ss AS (
     SELECT
         quantity,
         sale_date
     FROM {{ ref("base_postgres_sales") }}
 ),
 
-product as (
+pp as (
     SELECT
+        product_id,
         brand,
         model,
         color,
@@ -26,20 +30,21 @@ product as (
 )
 
 SELECT 
-        c.customer_id
-        c.first_name,
-        c.last_name,
-        c.gender,
-        c.country,
-        p.brand,
-        p.model,
-        p.color,
-        p.memory,
-        p.selling_price,
-        s.quantity,
-        s.sale_date
-FROM customers c
-    LEFT JOIN sales s
-    ON c.customer_id = s.customer_id
-    LEFT JOIN products p
-    ON s.product_id = p.product_id
+        cc.customer_id,
+        cc.first_name,
+        cc.last_name,
+        cc.gender,
+        cc.country,
+        pp.brand,
+        pp.model,
+        pp.color,
+        pp.memory,
+        pp.selling_price,
+        ss.quantity,
+        ss.sale_date
+FROM ss  JOIN 
+    cc ON cc.customer_id = customer_id
+JOIN 
+    pp ON pp.product_id = product_id
+    ORDER BY gender
+
